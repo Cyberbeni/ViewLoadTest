4dca5ccca2a6ea349dcfc9b8104cfca5376ca93e
Benedek Kozma <cyberbeni@gmail.com>
2018-06-30 13:02:33 +0200

    Initial Commit

---
+++ ViewLoadTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewLoadTest
+//
+//  Created by Benedek Kozma on 2018. 06. 30..
+//  Copyright © 2018. Benedek Kozma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
